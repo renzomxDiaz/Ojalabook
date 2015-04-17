8f606cbea369c291340452237f3c85d5b5f84086
Renzo Diaz <renzomx16@gmail.com>
2015-04-16 21:06:11 -0500

    Bootstrap e interactividad agregado

--- config/application.rb
+++ config/application.rb
@@ -55,7 +55,8 @@ module Ojalabook
 
     # Enable the asset pipeline
     config.assets.enabled = true
-
+    # Solved font path.
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
